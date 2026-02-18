ec06cdb0562c9089da62179a5a1feaf73d7fbda2
Mac <mac@Mimus-MacBook-Pro.local>
2026-02-18 11:40:18 +0600

    23-51156-1

---
+++ task/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[][] jarray = new int[3][];
+        jarray[0] = new int[] { 10, 12 };
+        jarray[1] = new int[] { 1, 2, 3, 4 };
+        jarray[2] = new int[] { 3, 4, 5 };
+
+        foreach (int[] subArray in jarray)
+        {
+            foreach (int b in subArray)
+            {
+                Console.Write(b + " ");
+            }
+        }
+
+        Console.WriteLine(); // Move to next line after printing all elements
+    }
+}
---
+++ task/using System;.cs
@@ -0,0 +1,21 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        double price = 250;
+        double discountRate = 0.15;
+        double discountAmount, finalPrice;
+
+        discountAmount = price * discountRate;
+        finalPrice = price - discountAmount;
+
+        Console.WriteLine($"Original Price: ${price}");
+        Console.WriteLine($"Discount: 15%");
+        Console.WriteLine($"Final Price after discount: ${finalPrice:F2}");
+
+
+
+    }
+}
